db8332902e5f4f62225205a9a32f3f48359f4f1a
Aditya Semwal <129389158+a2dity@users.noreply.github.com>
2025-01-19 21:28:36 +0530

    Update index.js

--- index/index.js
+++ index/index.js
@@ -3,7 +3,8 @@ const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
-async function loadMovies(searchTerm){
+async function loadMovies(
+    searchTerm){
     const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
